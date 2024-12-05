1358701e8a33e9ae1c4ef5d7876e33bf25bc216b
aarya-114 <aarya.patil@vit.edu.in>
2024-12-05 17:35:14 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,22 +1,14 @@
-const accountId = 144553
+const accountId = 144553   //cannot chnage
 let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
+var accountPassword = "12345"    //avoid to use it  because of issue in block scope and functional scope
 accountCity = "Jaipur"
-let accountState;
+let accountState;     //undefined value
 
-// accountId = 2 // not allowed
 
 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
-
-console.log(accountId);
-
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
