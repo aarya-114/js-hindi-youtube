ede2ddaf27d83d8a46b91a92afb4aeb1c2e21f05
aarya-114 <aarya.patil@vit.edu.in>
2024-12-05 17:41:45 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,12 +1,8 @@
 "use strict"; // treat all JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// alert( 3 + 3)        // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
 
-console.log("Hitesh")
 
 
 let name = "hitesh"
@@ -14,16 +10,20 @@ let age = 18
 let isLoggedIn = false
 let state;
 
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
+/*
+DATATYPES
+number => 2 to power 53
+bigint
+string => " "
+boolean => true/false
+null => standalone value (representation of empty value)
+undefined => (variable decalre but value not define)
+symbol => unique        more used in react.js 
+*/
+
 
 
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+console.log(typeof undefined);       // undefined
+console.log(typeof null);           // object
