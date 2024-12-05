65a67b0909662b235f5c3ba4e95eda56fe6360e8
aarya-114 <aarya.patil@vit.edu.in>
2024-12-05 18:08:28 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,25 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+
+//comparison    > >= < <= == !=
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);   TRUE
+// console.log("02" > 1);  TRUE
+//Sometime didnt give predictable result ....necessary to check the datatype
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);    false 
+// console.log(null == 0);   false
+// console.log(null >= 0);   true
+
+
+/*******
+equality heck == and comparison check works differently
+comparison convert null to number,treating as a 0
+**********/
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// ===  strict check...chevck VALUES & DATATYPES
 
 console.log("2" === 2);
