30a7edeffee63a43e1921bfdf048eca8c3e74683
aarya-114 <aarya.patil@vit.edu.in>
2024-12-06 17:14:59 +0530

    06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,21 +1,17 @@
-const score = 400
-// console.log(score);
-
+const score = 400 
 const balance = new Number(100)
-// console.log(balance);
+
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(1));            //to fixed the precsiion
 
 const otherNumber = 123.8966
-
 // console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
-
 // console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
@@ -24,11 +20,10 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
+console.log(Math.random());     //between 0 and 1
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
-
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
