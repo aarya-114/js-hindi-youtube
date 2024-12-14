4310984fc94e4dc303e905cc5bb9996741d7d222
aarya-114 <aarya.patil@vit.edu.in>
2024-12-14 10:03:52 +0530

    04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,8 @@
 // Immediately Invoked Function Expressions (IIFE)
+//Avoid polluting the global scope
+//sometimes needed when to excute file as sooon as an application is satrted
 
-
+//(--)()
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
